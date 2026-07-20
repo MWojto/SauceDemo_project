42de86514d59228342e5b7e67a66d9ae24b339ee
MWojto <wojtowiczmikolaj943@gmail.com>
2026-07-20 20:54:01 +0200

    Added tests that explores the work of sidebar

---
+++ tests/inventory_tests/sidebar_test.spec.js
@@ -0,0 +1,28 @@
+import {test, expect} from '@playwright/test'
+import { LoginPage } from '../../pages/login_page'
+
+test('About button interaction', async({page}) => {
+    const login = new LoginPage(page)
+
+    await login.goToLoginPage()
+    await login.login('standard_user','secret_sauce')
+    
+    await page.getByRole('button', { name: 'Open Menu' }).click();
+    await page.locator('[data-test="about-sidebar-link"]').click();
+    await expect(page).toHaveURL('https://saucelabs.com/')
+})
+
+test('All items button interaction', async({page}) => {
+    const login = new LoginPage(page)
+
+    await login.goToLoginPage()
+    await login.login('standard_user','secret_sauce')
+    
+    await page.locator('[data-test="shopping-cart-link"]').click();
+    await expect(page).toHaveURL('https://www.saucedemo.com/cart.html')
+
+    await page.getByRole('button', { name: 'Open Menu' }).click();
+    await page.locator('[data-test="inventory-sidebar-link"]').click();
+
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+})
